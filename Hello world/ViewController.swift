1f7a9c51772347e8110f00dca42aa64bb03ad899
Антон Редкозубов <uacreez@gmail.com>
2020-08-10 13:18:53 +0400

    Initial Commit

---
+++ Hello world/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hello world
+//
+//  Created by Anton Redkozubov on 10.08.2020.
+//  Copyright © 2020 Anton Redkozubov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
